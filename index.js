9cfa883eb7f1447a0600a5dee77e9d0b014d61e0
Rahul Chopra <scorpio.sk98@gmail.com>
2024-07-02 19:15:04 +0530

    backend setup

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ store.on("error", function (error) {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://parakh-frontend.netlify.app",
+      "https://chopra1511.github.io/Parakh_client",
+    ],
     credentials: true,
   })
 );
